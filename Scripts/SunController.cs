af6e6df95129b5ae39414fff396daeaa5b655a4d
Kaleb Lott <Kaleb Lott>
2016-02-11 14:55:55 -0700

    Assets from home, this will be the actual branch before master

---
+++ Scripts/SunController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunController : MonoBehaviour {
+
+    public TimeControl TimeController;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(TimeController.Delta, 0f, 0f);
+	}
+}
---
+++ Scripts/TimeControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeControl : MonoBehaviour {
+
+    public float DegreeOfTime = 0;
+    public float Modifier = 1;
+    public float Delta = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        DegreeOfTime += Time.deltaTime * Modifier;
+        Delta = Time.deltaTime * Modifier;
+	}
+}
