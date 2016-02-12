e67ef9044dffc2c44e858cd89289f81473c1d039
Kaleb Lott <Kaleb Lott>
2016-02-12 15:22:32 -0700

    Add in stuff that apparently didn't get merged

---
+++ Scripts/ShadowProjectionControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShadowProjectionControl : MonoBehaviour {
+
+	public Text text;
+
+	public void ToggleSetting () {
+		if (QualitySettings.shadowProjection == ShadowProjection.CloseFit) {
+			QualitySettings.shadowProjection = ShadowProjection.StableFit;
+			text.text = "Stable Fit";
+		} else {
+			QualitySettings.shadowProjection = ShadowProjection.CloseFit;
+			text.text = "Close Fit";
+		}
+	}
+}
